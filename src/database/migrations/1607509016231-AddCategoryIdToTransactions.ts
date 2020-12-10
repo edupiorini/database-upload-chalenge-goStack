940d1cf1d41f0e19e7c239121831fbc75664f0d1
Eduardo Piorini <eduardopiorini@hotmail.com>
2020-12-10 17:07:03 -0300

    challenge finished

--- src/database/migrations/1607509016231-AddCategoryIdToTransactions.ts
+++ src/database/migrations/1607509016231-AddCategoryIdToTransactions.ts
@@ -1,8 +1,38 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableForeignKey,
+  TableColumn,
+} from 'typeorm';
 
 export default class AddCategoryIdToTransactions1607509016231
   implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<any> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
 
-  public async down(queryRunner: QueryRunner): Promise<any> {}
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'TransactionCategory',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionCategory');
+
+    await queryRunner.dropColumn('transactions', 'category_id');
+  }
 }
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,8 +1,9 @@
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 import AppError from '../errors/AppError';
 
 import TransactionRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
 interface RequestDTO {
   title: string;
@@ -19,7 +20,20 @@ class CreateTransactionService {
     value,
   }: RequestDTO): Promise<Transaction> {
     const transactionRepository = getCustomRepository(TransactionRepository);
+    const categoryRepository = getRepository(Category);
 
+    let transactionCategory = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category,
+      });
+      await categoryRepository.save(transactionCategory);
+    }
+
+    // verificação de value e type
     if (!['income', 'outcome'].includes(type)) {
       throw new AppError('Invalid type!');
     }
@@ -31,10 +45,12 @@ class CreateTransactionService {
     if (type === 'outcome' && total < value) {
       throw new AppError('Not enough funds available');
     }
+
     const transaction = transactionRepository.create({
       title,
       value,
       type,
+      category: transactionCategory,
     });
 
     await transactionRepository.save(transaction);
